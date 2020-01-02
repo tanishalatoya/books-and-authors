309799334bf409892db5aac2bb3d63e1dbf9018f
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-01 20:56:17 -0700

    Pass book test - TDD

--- book.js
+++ book.js
@@ -1,5 +1,9 @@
 class Book {
-
+  constructor(firstName, lastName, title, publicationYear) {
+    this.title = title;
+    this.publicationYear = publicationYear;
+    this.author = `${firstName} ${lastName}`
+  }
 }
 
 module.exports = Book;
