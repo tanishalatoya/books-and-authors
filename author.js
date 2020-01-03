69bd627cabc1d4d9598631a5fa1365f7b6c724a2
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-02 19:32:42 -0700

    Pass author test - TDD

--- author.js
+++ author.js
@@ -1,4 +1,17 @@
+var Book = require('./book.js');
+
 class Author {
+  constructor(anAuthor) {
+    this.first = anAuthor.first;
+    this.last = anAuthor.last;
+    this.books = [];
+  }
+
+  write(title, publicationYear) {
+    var book = new Book(this.first, this.last, title, publicationYear);
+    this.books.push(book);
+    return book;
+    }
 
 }
 
